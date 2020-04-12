929fb63f94f3f8777d0ff66f75c0a0a03c7b1d4c
mohammad-adk <mohamad.adk78@gmail.com>
2020-04-12 23:57:16 +0430

    backend initialized

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+from flask_restful import Api
+from resources.user import User
+# from resources.Register import Register
+# from resources.Signin import Signin
+# from resources.Task import Tasks
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+# Route
+api.add_resource(User, '/user')
+# api.add_resource(Register, '/register')
+
+# api.add_resource(Signin, '/signin')
+
+# api.add_resource(Tasks, '/tasks')

5a0278965ecd63efdbe79b3b163520d3b10f4dfe
mohammad-adk <mohamad.adk78@gmail.com>
2020-04-14 15:16:28 +0430

    api key added

--- backend/migrate.py
+++ backend/migrate.py
@@ -1,6 +1,6 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import db
+from resources.models import db
 from run import create_app
 
 app = create_app('config')
